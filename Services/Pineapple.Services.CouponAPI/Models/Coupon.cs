ce3abe74affba9f8f717fc5ad471b920415288df
Oleksandr <Oleksandr@DESKTOP-3CK06JK>
2024-01-21 15:41:42 +0200

    starting realization of CouponAPI micriservice, installed Nuget Packages

---
+++ Services/Pineapple.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Pineapple.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinimalAmount { get; set; }
+    }
+}
